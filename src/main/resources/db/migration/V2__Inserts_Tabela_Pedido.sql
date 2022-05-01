e739e77623c198f6bf18c43ee2f2f82b9ea9bd13
Vitor Lucas <vitorluc.2013@gmail.com>
2022-05-01 19:54:12 -0300

    Populating the bank and adjusting the 'Order' table

---
+++ src/main/resources/db/migration/V2__Inserts_Tabela_Pedido.sql
@@ -0,0 +1,15 @@
+
+INSERT INTO Pedido (nome_Produto, valor_Negociado, data_da_Entrega, url_Produto, url_Imagem, descricao, status)
+VALUES ('Suporte Articulado de mesa', 249.80, '2022-01-09', 'https://www.amazon.com.br/gp/product/B08MRNFRVC/ref=ox_sc_act_image_1?smid=A1ZZFT5FULY4LN&psc=1', 'https://m.media-amazon.com/images/I/512HToxgaaL._AC_SX522_.jpg', 'UPORTE ARTICULADO DE MESA COM PISTÃO A GÁS PARA MONITORES DE 17" A 27" - PRETO - AF80N - ELG', 'ENTREGUE');
+
+
+INSERT INTO Pedido (nome_Produto, valor_Negociado, data_da_Entrega, url_Produto, url_Imagem, descricao, status)
+VALUES ('Balança Digital', 163.0, '2022-03-09', 'https://www.amazon.com.br/Ozeri-ZK14-S-Digital-Multifuncional-Alimentos/dp/B004164SRA/?_encoding=UTF8&pd_rd_w=q8d0G&pf_rd_p=7721c677-7472-42db-8338-bc52d43d62e7&pf_rd_r=D97W4K67EJMV15BP3GYF&pd_rd_r=82037217-6e42-4c93-bfc6-f682b215253d&pd_rd_wg=OUG5M&ref_=pd_gw_ci_mcx_mr_hp_d', 'https://m.media-amazon.com/images/I/71IMyNURFML._AC_SX522_.jpg', 'Balança Digital Multifuncional de Cozinha Ozeri ZK14-S Pronto, Alimentos, Preto', 'ENTREGUE');
+
+
+INSERT INTO Pedido (nome_Produto, valor_Negociado, data_da_Entrega, url_Produto, url_Imagem, descricao, status)
+VALUES ('Notebook Acer', 5749.0, null, 'https://www.amazon.com.br/Notebook-Acer-Intel-I7-10510u-512ssd/dp/B09HHQ8RS3/?_encoding=UTF8&pd_rd_w=72HLD&pf_rd_p=df4ac1cc-1218-4e71-aa2e-283172bce35c&pf_rd_r=16WDS0D1M6RFQVGY11XK&pd_rd_r=15d36550-c08c-4e77-9a1e-59baefce6781&pd_rd_wg=lxYET&ref_=pd_gw_ci_mcx_mi', 'https://m.media-amazon.com/images/I/31WZQmbeuZL._AC_.jpg', 'Notebook Acer Intel Core I7-10510u 20gb 512ssd+1tb 15,6 Fhd', 'AGUARDANDO');
+
+
+INSERT INTO Pedido (nome_Produto, valor_Negociado, data_da_Entrega, url_Produto, url_Imagem, descricao, status)
+VALUES ('SSD', 679.99, null, 'https://www.amazon.com.br/DESKTOP-NOTEBOOK-KINGSTON-SA400S37-960G/dp/B079XC5PVV/?_encoding=UTF8&pd_rd_w=72HLD&pf_rd_p=df4ac1cc-1218-4e71-aa2e-283172bce35c&pf_rd_r=16WDS0D1M6RFQVGY11XK&pd_rd_r=15d36550-c08c-4e77-9a1e-59baefce6781&pd_rd_wg=lxYET&ref_=pd_gw_ci_mcx_mi', 'https://m.media-amazon.com/images/I/81kVu+dAPTL._AC_SX679_.jpg', null, 'APROVADO');
